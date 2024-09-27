c21b84fdb843a6e5aba58ac8926d520a33d95122
hasibul1670 <hasibulislam1670@gmail.com>
2024-09-27 08:53:14 +0600

    change some icon

--- src/app/pages/sidebar/sidebar.component.ts
+++ src/app/pages/sidebar/sidebar.component.ts
@@ -2,6 +2,16 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import {
+  bootstrapArrowBarLeft,
+  bootstrapArrowBarRight,
+  bootstrapBarChartLine,
+  bootstrapBorderAll,
+  bootstrapCartPlus,
+  bootstrapChatText,
+  bootstrapPen,
+  bootstrapShopWindow,
+} from '@ng-icons/bootstrap-icons';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { heroUsers } from '@ng-icons/heroicons/outline';
 
@@ -11,6 +21,18 @@ import { heroUsers } from '@ng-icons/heroicons/outline';
   imports: [MatButtonModule, NgIconComponent, MatDividerModule, MatIconModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
-  viewProviders: [provideIcons({ heroUsers })],
+  viewProviders: [
+    provideIcons({
+      heroUsers,
+      bootstrapBorderAll,
+      bootstrapArrowBarRight,
+      bootstrapArrowBarLeft,
+      bootstrapChatText,
+      bootstrapPen,
+      bootstrapCartPlus,
+      bootstrapShopWindow,
+      bootstrapBarChartLine,
+    }),
+  ],
 })
 export class SidebarComponent {}
